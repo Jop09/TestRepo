020564a6cac1c3d6595ad048f6aab9c97a484047
unknown <Praktikant@IKSMOBIL1204.iks-gmbh.com>
2016-02-26 12:17:04 +0100

    initial commit

---
+++ kapitel1/GanzEinfach.java
@@ -0,0 +1,21 @@
+package kapitel1;
+
+public class GanzEinfach {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test");
+	}
+
+}
+
+
+
+
+//*************Hinweis************************************
+//
+//Die Originaldatei finden Sie auf der CD-ROM unter
+//work/Kap1/TestApplet.java
+//
+//*************Ende Hinweis*****************************
+
